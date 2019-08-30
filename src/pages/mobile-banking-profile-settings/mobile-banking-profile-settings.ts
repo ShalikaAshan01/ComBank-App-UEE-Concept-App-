63b32ea7c2e4c8dde9b30446697daa4ee228de0d
Shalika <shalikaashan01@gmail.com>
2019-08-30 10:41:03 +0530

    mobile banking profile settings page generated

--- src/pages/mobile-banking-profile-settings/mobile-banking-profile-settings.ts
+++ src/pages/mobile-banking-profile-settings/mobile-banking-profile-settings.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
- * Generated class for the MobileBankingProfileViewPage page.
+ * Generated class for the MobileBankingProfileSettingsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-mobile-banking-profile-view',
-  templateUrl: 'mobile-banking-profile-view.html',
+  selector: 'page-mobile-banking-profile-settings',
+  templateUrl: 'mobile-banking-profile-settings.html',
 })
-export class MobileBankingProfileViewPage {
+export class MobileBankingProfileSettingsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MobileBankingProfileViewPage');
+    console.log('ionViewDidLoad MobileBankingProfileSettingsPage');
   }
 
 }

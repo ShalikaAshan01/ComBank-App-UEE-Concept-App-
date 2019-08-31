851a8524df0f5c65bd04ddab2ca9e6609bc2145a
Pasan Godamune <pasanjg@gmail.com>
2019-08-31 08:56:27 +0530

    fund transfer updates and fixes

--- src/pages/fund-transfer/fund-transfer.ts
+++ src/pages/fund-transfer/fund-transfer.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 /**
  * Generated class for the FundTransferPage page.
@@ -15,20 +16,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FundTransferPage {
 
+  formGroup: FormGroup;
+
   options: string;
   userAccounts: any;
   thirdPartyAccounts: any;
 
-  type: string;
-  payFrom: any;
-  payTo: any;
-  amount: any;
+  type: AbstractControl;
+  payFrom: AbstractControl;
+  payTo: AbstractControl;
+  amount: AbstractControl;
+  date = new Date();
+  month: number;
+  currentDate: string;
+
+  notComplete: boolean = true;
 
   transfer: {};
   transferDetails: {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.options = 'transfer';
+    this.month = this.date.getMonth() + 1;
+    this.currentDate = this.date.getFullYear() + '/' + this.month + '/' + this.date.getMonth();
+
+    this.formGroup = formBuilder.group({
+      type: ['', Validators.required],
+      payFrom: ['', Validators.required],
+      payTo: ['', Validators.required],
+      amount: ['', Validators.required]
+    });
+
+    // this.type = this.formGroup.controls['type'];
+    // this.payFrom = this.formGroup.controls['payFrom'];
+    // this.payTo = this.formGroup.controls['payTo'];
+    // this.amount = this.formGroup.controls['amount'];
 
     this.userAccounts = [
       { 'currency': 'LKR', 'accountBalance': 107458.14, 'accountNumber': '8480065437', 'cardNumber': '5247 8745 6598 3541', 'cardName': 'Godamune G.A.P.J.', 'expiry': '05/24', 'accountType': 'Savings Account', 'cardType': 'VISA' },
@@ -43,9 +65,9 @@ export class FundTransferPage {
     ];
 
     this.transferDetails = [
-      { 'type': 'Own Account', 'paidFrom': '8480065437', 'paidTo': '8480065656', 'amount': '1000.00', 'date': '', 'status': 'successful' },
-      { 'type': 'Own Account', 'paidFrom': '8480067543', 'paidTo': '8480027893', 'amount': '1000.00', 'date': '', 'status': 'successful' },
-      { 'type': 'Third Party', 'paidFrom': '8480009875', 'paidTo': '8480086100', 'amount': '1000.00', 'date': '', 'status': 'successful' }
+      { 'type': 'Own Account', 'paidFrom': '8480065437', 'paidTo': '8480065656', 'amount': '1000.00', 'date': this.currentDate, 'status': 'successful' },
+      { 'type': 'Own Account', 'paidFrom': '8480067543', 'paidTo': '8480027893', 'amount': '1000.00', 'date': this.currentDate, 'status': 'successful' },
+      { 'type': 'Third Party', 'paidFrom': '8480009875', 'paidTo': '8480086100', 'amount': '1000.00', 'date': this.currentDate, 'status': 'successful' }
     ];
   }
 
@@ -59,9 +81,19 @@ export class FundTransferPage {
       'payFrom': this.payFrom,
       'payTo': this.payTo,
       'amount': this.amount,
-      'date': new Date()
+      'date': this.currentDate
     }
-    console.log(this.transfer)
+    this.formGroup.reset();
+    this.notComplete = false;
+    console.log(this.transfer);
+
+    setInterval(() => {
+      this.notComplete = true;
+    }, 5000);
+  }
+
+  okay() {
+    this.notComplete = true;
   }
 
 }

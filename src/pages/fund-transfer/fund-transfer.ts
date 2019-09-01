3929a3e97337ac7b105b3b239d915a7a233ce106
Pasan Godamune <pasanjg@gmail.com>
2019-09-01 16:54:40 +0530

    font updated

--- src/pages/fund-transfer/fund-transfer.ts
+++ src/pages/fund-transfer/fund-transfer.ts
@@ -73,6 +73,7 @@ export class FundTransferPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FundTransferPage');
+    // console.log(this.formatMoney(5000, '', '', ''));
   }
 
   makeTransfer() {
@@ -96,4 +97,17 @@ export class FundTransferPage {
     this.notComplete = true;
   }
 
+  // formatMoney(number, decPlaces, decSep, thouSep) {
+  //   decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+  //     decSep = typeof decSep === "undefined" ? "." : decSep;
+  //   thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+  //   var sign = number < 0 ? "-" : "";
+  //   var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+  //   var j = (j = i.length) > 3 ? j % 3 : 0;
+
+  //   return sign +
+  //     (j ? i.substr(0, j) + thouSep : "") +
+  //     i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+  //     (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+  // }
 }
